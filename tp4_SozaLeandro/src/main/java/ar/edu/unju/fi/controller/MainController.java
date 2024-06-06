30bbc16f189b92597fcdab6294cb4480622fad34
Leandro Soza <leancarp0495@gmail.com>
2024-06-06 18:54:54 -0300

    Creacion layouts con header y footer

--- tp4_SozaLeandro/src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ tp4_SozaLeandro/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -1,12 +1,14 @@
 package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	@GetMapping("/index")
-	public String getIndexPage() {
+	@GetMapping("/inicio")
+	public String getIndexPage(Model model) {
+		model.addAttribute("titulo", "Inicio");
 		return "index";
 	}
 }
